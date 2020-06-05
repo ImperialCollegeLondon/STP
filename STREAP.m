8264524bc5f357aca7b3123dd2a7bebe152c092c
TitianChen <yuting.chen17@imperial.ac.uk>
2020-06-05 16:59:02 +0100

    remove NHPO feature in generation

--- STREAP.m
+++ STREAP.m
@@ -1,47 +1,50 @@
 % STREAP Main code
-
 tic
 %% Initilazing
 rng('shuffle');
 addpath(genpath(cd))
 %% PreProcessing
 %% Load data
-load('MeanArealStats.mat') % WAR [-], (CAR [-]) and IMF [mm h^-1] data
+filePath = 'Birmingham/';%'Engelberger/';%
+load([filePath,'MeanArealStats.mat']) % WAR [-], (CAR [-]) and IMF [mm h^-1] data
 % U500, V500: [m s^-1]
 % NO MeanArealStats.CAR;
-load('CV.mat') % Rainfall coefficient of variation [-], monthly data
-load('ARMA.mat') % ARMA coefficients
-load('Gauge_10_min_precip.mat', 'MERGED') % Boolean rain / no-rain (10-min interval), composite of 4 MeteoSwiss rain-gauge data
-load('expoSpatialCorrelation.mat') % Exponential coefficient of the spatial correlation
-load('NHRO.mat') % Non homogenic rainfall ocurrence; in this example number of days above a certain threshold, can be also hours or minutes
-load('gpFitSO.mat') % Generalized Pareto parameters for the observed and simulated daily data
-load('thresholdI.mat') % Simulated daily rain intensity threshold [mm] (from which daily rainfall > 1 mm)
-% load('LRdata.mat') % Temperature data for two stations to calculate lapse rate
-% load('CloudCover.mat') % Hourly cloud cover data for the temperature analysis
-% load('Ta_Stations.mat') % Temperature stations located within the domain
-% load('Rsw_Stations.mat') % Short wave radiation from stations
-% load('Ps_Stations.mat') % Pressure at ground level from stations
-% load('AOD.mat') % AERONET aerosol optical depth data
-domainDTM=rasterread('engelberger_DTM.txt'); % 250 x 250 pixels [100 x 100 m] DTM of the Engelberger catchment
-domainDTM=flipud(domainDTM);
-
+load([filePath,'CV.mat']) % Rainfall coefficient of variation [-], monthly data
+load([filePath,'ARMA.mat']) % ARMA coefficients
+%
+% load([filePath,'Gauge_10_min_precip.mat'], 'MERGED') % Boolean rain / no-rain (10-min interval), composite of 4 MeteoSwiss rain-gauge data
+%
+load([filePath,'expoSpatialCorrelation.mat']) % Exponential coefficient of the spatial correlation
+load([filePath,'NHRO.mat']) % Non homogenic rainfall ocurrence; in this example number of days above a certain threshold, can be also hours or minutes
+%
+% load([filePath,'gpFitSO.mat']) % Generalized Pareto parameters for the observed and simulated daily data
+% load([filePath,'thresholdI.mat']) % Simulated daily rain intensity threshold [mm] (from which daily rainfall > 1 mm)
+%
+dx = 1;%2
+dt = 5;
+domainSize = 110;%13
+WAR_threshold = 0.1;%0.02;
 %% Rainfall event analysis - gauged based
-[ gaugeIsEvent ] = findGaugeEvents( MERGED(:,1) , 0.1 , 10 ); 
-gaugeIsEvent(:,2) = MERGED(:,2);
-save('Files\gaugeIsEvent.mat','gaugeIsEvent','-v7.3');
-% Boolean vector defining an event for each 10-min; in this case, there is no meaning for the rainfall threshold (set arbitrary to 0.1 [mm]). 
+% isEventdt = 10;%10min
+% [ gaugeIsEvent ] = findGaugeEvents( MERGED(:,1) , 0.1 , isEventdt ); 
+% gaugeIsEvent(:,2) = MERGED(:,2);
+% save('Files\gaugeIsEvent.mat','gaugeIsEvent','-v7.3');
+% Boolean vector defining an event for each 10-min; in this case, 
+% there is no meaning for the rainfall threshold (set arbitrary to 0.1 [mm]). 
 % Similar file exists for radar analysis: find RadarEvents
-[ radarIsEvent ] = findRadarEvents( MeanArealStats.WAR , 0.02 , 60 );
+[ radarIsEvent ] = findRadarEvents( MeanArealStats.WAR , WAR_threshold , 60 );
 radarIsEvent(:,2) = MeanArealStats.Time;
 save('Files\radarIsEvent.mat','radarIsEvent','-v7.3');
-
+isEventdt = 5;
 %% Storm arrival process
 % Fitting distributions for the wet and dry periods
 for m=1:12
     Precipitation.data(m).isWetEventG=radarIsEvent(month(radarIsEvent(:,2))==m);
     Precipitation.data(m).isWetEventGyear=year(radarIsEvent(month(radarIsEvent(:,2))==m,2));
     Precipitation.data(m).isWetEventG(Precipitation.data(m).isWetEventG>1)=1;
-    [ Precipitation.data(m).dryG , Precipitation.data(m).wetG , Precipitation.data(m).dryFitG , Precipitation.data(m).wetFitG ] = WetDryG( Precipitation.data(m).isWetEventG , Precipitation.data(m).isWetEventGyear , 1 , 10 );
+    [ Precipitation.data(m).dryG , Precipitation.data(m).wetG , ...
+        Precipitation.data(m).dryFitG , Precipitation.data(m).wetFitG ] = ...
+        WetDryG( Precipitation.data(m).isWetEventG , Precipitation.data(m).isWetEventGyear , 1 , isEventdt );
     if m==1
         export_fig Figs\tif\Wet_Dry_Fit_example.tif -tif -transparent -r300
     end
@@ -51,8 +54,10 @@ end
 %% Rainfall spatial correlation
 % Finding alpha parameter for the FFT filter that match the spatial correlation exponential
 % coefficient
+% 13*2-1 % 110*2-1
+dim = domainSize*2-1;%25;
 parfor m=1:12
-    [ tmp{m} ] = fftAutoCorr( [25 25] , [1 20] , 2 , expoSpatialCorr(m));
+    [ tmp{m} ] = fftAutoCorr( [dim dim] , [1 40] , dx , expoSpatialCorr(m));
 end
 for m=1:12
     Precipitation.data(m).SpatialAlpha=tmp{m}(2);
@@ -60,11 +65,12 @@ end
 clear tmp
 
 %% Fit IMF distribution
-IMF=MeanArealStats.IMF(MeanArealStats.IMF>0&MeanArealStats.WAR>=0.1); % Lower threshold of WAR>=0.1 and IMF>0 is set for the IMF statistics
-T=MeanArealStats.Time(MeanArealStats.IMF>0&MeanArealStats.WAR>=0.1);
+IMF=MeanArealStats.IMF(MeanArealStats.IMF>0&MeanArealStats.WAR>=WAR_threshold); 
+% Lower threshold of WAR>=0.1 and IMF>0 is set for the IMF statistics
+T=MeanArealStats.Time(MeanArealStats.IMF>0&MeanArealStats.WAR>=WAR_threshold);
 for m=1:12
     Precipitation.IMF(m).IMF=IMF(month(T)==m);
-    [ ~ , ~ , ~ , ~ , MGGP , ~ , ~ ] = fitIMF( Precipitation.IMF(m).IMF , 1 ); % For this example the Mixed Gamma and aGeneralized Pareto distribution is applied; other distributions can be applied using this code (Gamma, Generalized Pareto , Mixed Exponential and LogNormal
+    [ ~ , ~ , ~ , ~ , MGGP , ~ , ~ ] = fitIMF( Precipitation.IMF(m).IMF(:) , 1 ); % For this example the Mixed Gamma and aGeneralized Pareto distribution is applied; other distributions can be applied using this code (Gamma, Generalized Pareto , Mixed Exponential and LogNormal
     Precipitation.IMF(m).case=5; % Out of 5 possible choices
     Precipitation.IMF(m).name='Mixed Gamma and Generalized Pareto';
     Precipitation.IMF(m).fit=MGGP;
@@ -76,7 +82,8 @@ end
 clear MGGP IMF
 
 %% Fit WAR distribution
-WAR=MeanArealStats.WAR(MeanArealStats.IMF>0&MeanArealStats.WAR>=0.1); % Lower threshold of WAR>=0.1 and IMF>0 is set for the IMF statistics
+WAR=MeanArealStats.WAR(MeanArealStats.IMF>0&MeanArealStats.WAR>=WAR_threshold); 
+% Lower threshold of WAR>=0.1 and IMF>0 is set for the IMF statistics
 for m=1:12
     Precipitation.WAR(m).WAR=WAR(month(T)==m);
     [ ~ , ~ , John , ~ , ~] = fitWAR( Precipitation.WAR(m).WAR , 1 ); % For this example the Johnson distribution is applied; Beta or LogNormal distributions can be also applied using this code
@@ -97,11 +104,10 @@ end
 save('Files\Precipitation','-struct','Precipitation','-v7.3')
 
 IsEvent=zeros(size(MeanArealStats.IMF));
-IsEvent(MeanArealStats.IMF>0&MeanArealStats.WAR>=0.1)=1; % Same thresholds as for the WAR and CAR assigned before
+IsEvent(MeanArealStats.IMF>0&MeanArealStats.WAR>=WAR_threshold)=1; % Same thresholds as for the WAR and CAR assigned before
 
 %% Estimating Varfima parameters
 [ IsEventID ] = isEventID( IsEvent );
-% [ Norm , Copula , MaternCov ] = triNormTransform( IsEventID , MeanArealStats.IMF , MeanArealStats.WAR , MeanArealStats.CAR , MeanArealStats.Time , Precipitation , Cloud );
 [ Norm , Copula , MaternCov ] = biNormTransform( IsEventID , MeanArealStats.IMF , MeanArealStats.WAR , MeanArealStats.Time , Precipitation);
 save('Files\biNormTrans.mat','Norm','Copula','MaternCov','-v7.3')
 
@@ -144,10 +150,9 @@ save('Files\Advection.mat','-struct','Advection','-v7.3')
 % of precipitation occurrence is therefore assume to follow the annual
 % rainfall filter. ALTERNATIVLY - one can choose to apply the NHRO daily
 % filter (as presented in AWE-2d paper)
-load('obsAnnualRain.mat')%13*13 grids
-domainSize = 13;
+load([filePath,'obsAnnualRain.mat'])%13*13 grids
 ri=zeros(domainSize,domainSize);
-for y=1:32
+for y=1:length(ObsAnnualRain)
     ri=ri+ObsAnnualRain{y};
 end
 ri=ri./y;
@@ -159,27 +164,30 @@ clear tmp y m
 save('Files\obsOCC.mat','obsOCC','-v7.3');
 
 %% Processing
-
-load('Files\gaugeIsEvent.mat','gaugeIsEvent');
+load('Files\radarIsEvent.mat','radarIsEvent');
 load('Files\obsOCC.mat','obsOCC');
 Advection = load('Files\Advection.mat');
 load('Files\biNormTrans.mat','Norm','Copula','MaternCov')
 Precipitation = load('Files\Precipitation.mat');
-
-I=1; % Number of realizations to simulate (the processing stage can be looped)
-
+load([filePath, 'CV.mat'])
+I = 1; % Number of realizations to simulate (the processing stage can be looped)
 %% Storm arrival process
 [ dryPool , wetPool ] = drywetPool( I , Precipitation.data );
-[ tsMonth , tsWetDry , wetPool , dryPool ] = tsWetDryMinG( wetPool , dryPool , 10/(24*60) );
-for i=1:length(tsWetDry) % For this example the 5-min temporal resolution is interpolated from the 10-min resolution
-    tsWetDry5Min(i*2-1)=tsWetDry(i);
-    tsWetDry5Min(i*2)=tsWetDry(i);
-    tsMonth5Min(i*2-1)=tsMonth(i);
-    tsMonth5Min(i*2)=tsMonth(i);
+if isEventdt == 5    
+    [ tsMonth , tsWetDry , wetPool , dryPool ] = tsWetDryMinG( wetPool , dryPool , dt/(24*60) );
+else
+    % for 10min gauge data % deleted by Yt
+    [ tsMonth , tsWetDry , wetPool , dryPool ] = tsWetDryMinG( wetPool , dryPool , isEventdt/(24*60) );
+    for i=1:length(tsWetDry) % For this example the [dt]-min temporal resolution is interpolated from the [dt*2]-min resolution
+        tsWetDry5Min(i*2-1)=tsWetDry(i);
+        tsWetDry5Min(i*2)=tsWetDry(i);
+        tsMonth5Min(i*2-1)=tsMonth(i);
+        tsMonth5Min(i*2)=tsMonth(i);
+    end
+    tsWetDry=tsWetDry5Min;
+    tsMonth=tsMonth5Min;
+    clear tsMonth5Min tsWetDry5Min
 end
-tsWetDry=tsWetDry5Min;
-tsMonth=tsMonth5Min;
-clear tsMonth5Min tsWetDry5Min
 [ tsMatrix ] = tsTable( tsWetDry , tsMonth );
 %% Varfima - simulating WAR, IMF
 simWAR=zeros(tsMatrix(end),1);
@@ -205,8 +213,6 @@ for i=1:size(tsMatrix,1)
     end
 end
 clear tsWARFIMA m
-
-
 %% Simulating advection
 simU=zeros(tsMatrix(end),1);
 simV=zeros(tsMatrix(end),1);
@@ -214,6 +220,9 @@ parfor i=1:size(tsMatrix,1)
     Sd{i}=simulate(Advection.Dry(tsMatrix(i,2)).mdl,20000);
     Sw{i}=simulate(Advection.Wet(tsMatrix(i,2)).mdl,20000);
 end
+poolobj = gcp('nocreate');
+delete(poolobj);
+
 progressbar('Generating advection components') % Init single bar
 for i=1:size(tsMatrix,1)
     m=tsMatrix(i,2);
@@ -268,60 +277,74 @@ end
 simU=single(simU);
 simV=single(simV);
 clear S Ss Sd Sw
-
 %% Generating rain fields
-dx = 2;%2Km
-dt = 5;%5min
+ARMA22 = ARMA(2);%ARMA(8);
+% ARMA22 = ARMA;
 simRain=zeros(105120,domainSize,domainSize);
 QField = [];
 progressbar('Generating rain fields') % Init single bar
+tic
 for m=1:12
+    tic
     mmin(m,1)=min(tsMatrix(tsMatrix(:,2)==m,3));
     mmax(m,1)=max(tsMatrix(tsMatrix(:,2)==m,4));
-    [ QField(mmin(m,1):mmax(m,1),:,:) ] = quantileFieldGen( [domainSize*2 domainSize*2] , ARMA , 5 , ...
+    [ QField(mmin(m,1):mmax(m,1),:,:) ] = quantileFieldGen( [domainSize*2 domainSize*2] , ARMA22 , 5 , ...
         Precipitation.data(m).SpatialAlpha , [mmin(m,1) mmax(m,1)] , simU , simV , dx , dt );
-    %% Non-homogeneous probability of precipitation occurrence
+    % Non-homogeneous probability of precipitation occurrence
+    % for i=mmin(m,1):mmax(m,1)
+    %     [ simRain(i,:,:) ] = NHPO( squeeze(QField(i,:,:)) , simWAR(i) , obsOCC{m});%%%obsOCC{m} );
+    % end
+    % Homogeneous probability of precipitation occurrence
     for i=mmin(m,1):mmax(m,1)
-        [ simRain(i,:,:) ] = NHPO( squeeze(QField(i,:,:)) , simWAR(i) , obsOCC{m} );
+         [ simRain(i,:,:) ] = NHPO( squeeze(QField(i,:,:)) , simWAR(i) , obsOCC{m}*0+1);%%%obsOCC{m} );
     end
     %% Assigning rainfall intensity for the Gaussian field
     for i=1:size(tsMatrix,1)
         M=tsMatrix(i,2);
         if tsMatrix(i,1)==1 && m==M
-            [ simRain(tsMatrix(i,3):tsMatrix(i,4),:,:) ] = invLN2( simRain(tsMatrix(i,3):tsMatrix(i,4),:,:) , simWAR(tsMatrix(i,3):tsMatrix(i,4)) , simIMF(tsMatrix(i,3):tsMatrix(i,4)) , CV(m));
+            [ simRain(tsMatrix(i,3):tsMatrix(i,4),:,:) ] = invLN2( simRain(tsMatrix(i,3):tsMatrix(i,4),:,:) , ...
+                simWAR(tsMatrix(i,3):tsMatrix(i,4)) , simIMF(tsMatrix(i,3):tsMatrix(i,4)) , CV(m));
         end
     end
+    toc
     progressbar(m/12) % Update progress bar
-end
-
-%% Non-homogeneous spatial rainfall accumulation
-% Simulation is divided to daily time steps to match the correction records
-t=(datenum(2001,1,1,0,0,0):1:datenum(2001,12,31,23,55,0))'; % One year dates based on the year 2001
-tmp=squeeze(sum(reshape(simRain,[288 365 13 13]))).*5/60;
-for i=1:13*13
-    for d=1:365
-        if tmp(d,i)<thresholdI(i)
-            k=tmp(d,i)/thresholdI(i);
-            simRain(1+288*(d-1):288*d,i)=simRain(1+288*(d-1):288*d,i).*k; % Below the 1 mm threshold is consider as "drizzle"
-            tmp(d,i)=0;
-        end
-    end
-end
-for d=1:365
-    m=month(t(d));
-    tmp2=squeeze(tmp(d,:,:));
-    gpCDF=gpcdf(tmp2,gpFitS{1}{m},gpFitS{2}{m},thresholdI);
-    gpCDF(gpCDF>0.999)=0.999;
-    gpINV=gpinv(gpCDF,gpFitO{1}{m},gpFitO{2}{m},1);
-    k=gpINV./tmp2; % Daily rainfall correction factor
-    k(isinf(k))=1;
-    simRain(1+288*(d-1):288*d,:,:)=simRain(1+288*(d-1):288*d,:,:).*repmat(reshape(k,[1 domainSize domainSize]),288,1,1);
 end
 simRain(isnan(simRain))=0;
 simRain=single(simRain);
+toc
+% %% Non-homogeneous spatial rainfall accumulation
+% % Simulation is divided to daily time steps to match the correction records
+% t=(datenum(2001,1,1,0,0,0):1:datenum(2001,12,31,23,55,0))'; % One year dates based on the year 2001
+% tmp=squeeze(sum(reshape(simRain,[24*60/dt 365 domainSize domainSize]))).*dt/60;
+% for i=1:domainSize*domainSize
+%     for d=1:365
+%         if tmp(d,i)<thresholdI(i)
+%             k=tmp(d,i)/thresholdI(i);
+%             simRain(1+24*60/dt*(d-1):24*60/dt*d,i)=simRain(1+24*60/dt*(d-1):24*60/dt*d,i).*k; % Below the 1 mm threshold is consider as "drizzle"
+%             tmp(d,i)=0;
+%         end
+%     end
+% end
+% for d=1:365
+%     m=month(t(d));
+%     tmp2=squeeze(tmp(d,:,:));
+%     gpCDF=gpcdf(tmp2,gpFitS{1}{m},gpFitS{2}{m},thresholdI);
+%     gpCDF(gpCDF>0.999)=0.999;
+%     gpINV=gpinv(gpCDF,gpFitO{1}{m},gpFitO{2}{m},1);
+%     k=gpINV./tmp2; % Daily rainfall correction factor
+%     k(isinf(k))=1;
+%     simRain(1+24*60/dt*(d-1):24*60/dt*d,:,:)=simRain(...
+%         1+24*60/dt*(d-1):24*60/dt*d,:,:).*repmat(reshape(k,[1 domainSize domainSize]),24*60/dt,1,1);
+% end
+% simRain(isnan(simRain))=0;
+% simRain=single(simRain);
 
 %% Saving output
 save('Files\Output_Pr_Clt_500UV.mat','simRain','simIMF','simWAR','simU','simV','-v7.3'); % Matlab
-clear simRain
-%%
+% clear simRain
+
 toc
+
+pause(10);
+run('H:\CODE_MATLAB\AWE-GEN-2D\Video_Map_STREAP.m')
+
